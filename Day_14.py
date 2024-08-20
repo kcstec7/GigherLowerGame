137b4bf3fe00da410bf4eab69db805e205397fce
Karla Santos <67128948+kcstec7@users.noreply.github.com>
2024-08-20 10:58:35 -0300

    Add files via upload

---
+++ Day_14.py
@@ -0,0 +1,78 @@
+# Exercise Higher Lower Game
+
+import os
+from Day_14_Art import logo, vs
+from Day_14_Data import data
+from random import randint
+
+def print_header():
+    os.system('cls')
+    print(logo)
+
+def pick_people():
+
+    random_index_1 = randint(0, number_of_records - 1)
+    random_option_A = data[random_index_1]
+
+    random_option_B = {}
+    while random_option_B == {}:
+        random_index_2 = randint(0, number_of_records - 1)
+
+        if random_index_2 != random_index_1:
+            random_option_B = data[random_index_2]
+
+    return random_option_A, random_option_B
+
+def print_description(description):
+
+    if (description[0].lower() == 'a' or
+            description[0].lower() == 'e' or
+            description[0].lower() == 'i' or
+            description[0].lower() == 'o' or
+            description[0].lower() == 'u'):
+        return "an " + description
+    else:
+        return "a " + description
+def get_details(random_option_A, random_option_B):
+
+    print(f"Compare A: {random_option_A["name"]}, {print_description(random_option_A["description"])}, from {random_option_A["country"]}")
+    print(vs)
+    print(f"Against B: {random_option_B["name"]}, {print_description(random_option_B["description"])}, from {random_option_B["country"]}\n\n")
+
+    followers_A = random_option_A["follower_count"]
+    followers_B = random_option_B["follower_count"]
+
+    # print(f"Followers A: {followers_A}, a {followers_B}")
+
+    return followers_A, followers_B
+
+def guess_and_check(followers_A, followers_B):
+
+    guess = ""
+    while guess != "A" and guess != "B":
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+    if guess == "A" and followers_A > followers_B:
+        return "R"  # For RIGHT
+    elif guess == "B" and followers_B > followers_A:
+        return "R"  # For RIGHT
+    else:
+        return "W"  # For WRONG
+
+print_header()
+number_of_records = len(data)
+score = 0
+continue_game = True
+
+while continue_game:
+
+    option_A, option_B = pick_people()
+    num_followers_A, num_followers_B = get_details(option_A, option_B)
+
+    if guess_and_check(num_followers_A, num_followers_B) == "R":
+        score += 1
+        print_header()
+        print(f"You're right! Current score: {score}\n")
+    else:
+        print(f"\nSorry, that's wrong. Final score: {score}")
+        continue_game = False
